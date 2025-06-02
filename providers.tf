3d00cd6a34f3386c831c0f69e23e684a155621b6
amitpustode <arpustode@gmail.com>
2025-06-02 17:46:40 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -26,8 +26,8 @@ terraform {
 provider "azurerm" {
   features {}
   #skip_provider_registration = true  //# Optional: skip auto-registration of resource providers (can speed up things, but not always recommended)
-  subscription_id = "715512ac-92a9-409c-8f40-6fe60ab0f1e7"
-  tenant_id = "28367600-f4b9-456b-9969-d665d0e5f1aa "
-  client_id = "c7961855-8184-43ce-82e0-1bd52ae33cef"
-  client_secret = "Azk8Q~Lqrq7c6h9blOCPvIMc.q3bE.eobw8~ibqz"
+  subscription_id = "xxxxxx-xxxx-xxxxx-xxxxx-xxxxxxx"
+  tenant_id = "xxxxxx-xxxx-xxxxx-xxxxx-xxxxxxx"
+  client_id = "xxxxxx-xxxx-xxxxx-xxxxx-xxxxxxx"
+  client_secret = "xxxxxx-xxxx-xxxxx-xxxxx-xxxxxxx"
 }
