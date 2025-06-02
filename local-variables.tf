7b0c727ccd3b339ed35d276e6384437a28ba1a49
amitpustode <arpustode@gmail.com>
2025-06-02 13:43:40 +0530

    Delete local-variables.tf

--- local-variables.tf
+++
@@ -1,9 +0,0 @@
-resource "random_id" "suffix" {
-  byte_length = 3
-}
-
-locals {
-  name_prefix  = "${var.product_name}-${var.environment}-${var.resource_group_location}"
-  dns_label    = "${var.product_name}-${random_id.suffix.hex}"
-  #storage_account_name_prefix = "${var.product_name}${var.environment}"
-}
