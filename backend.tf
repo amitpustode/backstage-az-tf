48ce0f7f9299b085d9f8cf77bfb0931f55ba5948
amitpustode <arpustode@gmail.com>
2025-06-09 14:26:06 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,11 @@
-#terraform {
-#  backend "azurerm" {}
-#}
+terraform {
+  backend "azurerm" {
+    resource_group_name = "test-rg"
+    storage_account_name = "test-sa"
+    container_name = "test-ca"
+    key = "dev/my.tfstate"
+    access_key = "xxxxxx-xxxx-xxxxx-xxxxx-xxxxxxx"
+  }
+}
 
-resource_group_name = "test-rg"
-storage_account_name = "test-sa"
-container_name = "test-ca"
-key = "dev/my.tfstate"
-access_key = "xxxxxx-xxxx-xxxxx-xxxxx-xxxxxxx"
+
